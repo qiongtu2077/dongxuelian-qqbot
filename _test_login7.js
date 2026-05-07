b263abaeab507441ec3d3b3e0483cb699d260398
Forever-zh-233 <3514272382@qq.com>
2026-05-07 13:55:38 +0800

    chore: 移除测试文件，加入gitignore

--- _test_login7.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const data = JSON.stringify({ password: 'lianlian' })
-const req = http.request({ hostname: '127.0.0.1', port: 5150, path: '/dashboard/api/login', method: 'POST', headers: { 'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data) } }, (res) => {
-  let body = ''
-  res.on('data', c => body += c)
-  res.on('end', () => { console.log(body); process.exit(0) })
-})
-req.write(data)
-req.end()
